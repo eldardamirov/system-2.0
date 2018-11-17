80938f746cd74603886c6e99e7a715f16cbc886a
eldardamirov <eldardamirov75@mail.ru>
2018-11-17 17:36:54 +0300

    commands.h file added.

---
+++ system 2.0/commands.h
@@ -0,0 +1,22 @@
+//
+//  commands.h
+//  system 2.0
+//
+//  Created by Эльдар Дамиров on 17.11.2018.
+//  Copyright © 2018 Эльдар Дамиров. All rights reserved.
+//
+
+#ifndef commands_h
+#define commands_h
+
+#define DEF_CMD (name, number) CMD_##name = number
+
+enum commands
+    {
+    #include "asm.h"
+    
+    };
+
+#undef DEF_CMD    
+
+#endif /* commands_h */
