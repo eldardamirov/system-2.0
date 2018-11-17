9f558939a181286ce05dfac3e57983a7a525c027
eldardamirov <eldardamirov75@mail.ru>
2018-11-17 17:18:42 +0300

    Initial Commit

---
+++ system 2.0/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  system 2.0
+//
+//  Created by Эльдар Дамиров on 17.11.2018.
+//  Copyright © 2018 Эльдар Дамиров. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
