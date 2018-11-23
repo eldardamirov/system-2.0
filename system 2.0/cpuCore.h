e86edd0833b01f8785557066aa84b1a3f53aaeff
eldardamirov <eldardamirov75@mail.ru>
2018-11-24 00:24:22 +0300

    cpuCore file added.

---
+++ system 2.0/cpuCore.h
@@ -0,0 +1,20 @@
+//
+//  cpuCore.h
+//  system 2.0
+//
+//  Created by Эльдар Дамиров on 24.11.2018.
+//  Copyright © 2018 Эльдар Дамиров. All rights reserved.
+//
+
+#ifndef cpuCore_h
+#define cpuCore_h
+
+
+class cpuCore
+    {
+    
+    };
+
+
+
+#endif /* cpuCore_h */
